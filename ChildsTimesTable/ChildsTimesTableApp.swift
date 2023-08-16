9386219087409dcc1bd11032716193a7a73138b6
Eugene Cross <eugene.cross@gmail.com>
2023-08-16 16:54:22 +0100

    Initial Commit

---
+++ ChildsTimesTable/ChildsTimesTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChildsTimesTableApp.swift
+//  ChildsTimesTable
+//
+//  Created by Eugene on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChildsTimesTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
