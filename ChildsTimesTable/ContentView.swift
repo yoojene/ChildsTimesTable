e1fdf87614c08e6bad12ee522e04128344f4a596
Eugene Cross <eugene.cross@gmail.com>
2023-08-19 21:45:25 +0100

    adding Question struct

--- ChildsTimesTable/ContentView.swift
+++ ChildsTimesTable/ContentView.swift
@@ -22,9 +22,29 @@ extension View {
 }
 
 
+struct Questions {
+    var questions: Array<String> = [""]
+    var answers: Array<Int> = [0]
+    
+    mutating func generateQuestions() {
+        for i in 2...12 {
+            for j in 1...12 {
+                
+                questions.append("What is \(i) x \(j)?")
+                answers.append((i * j))
+            }
+        }
+        
+        questions.remove(at: 0)
+        answers.remove(at: 0)
+    }
+    
+}
+
 
 struct ContentView: View {
     
+    @State private var gameStarted = false
     @State private var chosenTimesTable = 2
     @State private var totalQuestions = 5
     @State private var questions: Array<String> = [""]
@@ -48,7 +68,6 @@ struct ContentView: View {
 
                 
                 VStack {
-                    
                     Section {
                         Text("Choose a times table to practice").font(.headline)
                         Stepper("\(chosenTimesTable) times table", value: $chosenTimesTable, in: 2...12, step: 1 )
@@ -59,125 +78,129 @@ struct ContentView: View {
                         
                     }
                     Spacer()
-                    
-                    VStack {
-                        Spacer()
-                        Text(questions[questionNumber]).font(.title).animation(.default)
-                        Spacer()
-                        Text("\(answer)").font(.title)
-                        .focused($inputValueIsFocused)
-                        Spacer()
+                    if gameStarted {
+                        
+                        VStack {
+                            Spacer()
+                            Text(questions[questionNumber]).font(.title).animation(.default)
+                            Spacer()
+                            Text("\(answer)").font(.title)
+                            .focused($inputValueIsFocused)
+                            Spacer()
 
 
-                    }.multilineTextAlignment(.center)
-                        .frame(width: 250, height: 100)
-                        .background(.ultraThickMaterial)
-                        .overlay(RoundedRectangle(cornerSize: CGSize(width: 25, height: 25))
-                            .stroke(Color.black, lineWidth: 1.0))
-                        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 25, height: 25)))
-                    
-                    .onSubmit {
-                        checkAnswer()
+                        }.multilineTextAlignment(.center)
+                            .frame(width: 250, height: 100)
+                            .background(.ultraThickMaterial)
+                            .overlay(RoundedRectangle(cornerSize: CGSize(width: 25, height: 25))
+                                .stroke(Color.black, lineWidth: 1.0))
+                            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 25, height: 25)))
+                        
+                        .onSubmit {
+                            checkAnswer()
+                        }
+                        .alert(isPresented: $showingScore) {
+                            Alert(
+                                title: Text(answer == answers[questionNumber] ? "Correct" : "Wrong"),
+                                message: Text("You have \(score) points in total"),
+                                dismissButton: .default(Text("Continue")) {
+                                    nextQuestion()
+                                })
+                        }
+                        
+                        Spacer()
+                        
+                        
+                        VStack (spacing: 20) {
+                            HStack (spacing: 20) {
+                                Button("1") {
+                                    
+                                    buttonPadAnswers.append("1")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 1
+
+                                }
+                                    .buttonPadStyle()
+                                Button("2") {
+                                    
+                                    buttonPadAnswers.append("2")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 2
+                                }
+                                    .buttonPadStyle()
+                                Button("3") {
+                                    buttonPadAnswers.append("3")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 3
+                                }
+                                    .buttonPadStyle()
+                            }
+                            HStack (spacing: 20 ){
+                                Button("4") {
+                                    buttonPadAnswers.append("4")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 4
+
+                                }
+                                    .buttonPadStyle()
+                                Button("5") {
+                                    buttonPadAnswers.append("5")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 5
+
+                                }
+                                    .buttonPadStyle()
+                                Button("6") {
+                                    buttonPadAnswers.append("6")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 6
+
+                                }
+                                    .buttonPadStyle()
+                            }
+                            HStack (spacing: 20) {
+                                Button("7") {
+                                    buttonPadAnswers.append("7")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 7
+
+                                }
+                                    .buttonPadStyle()
+                                Button("8") {
+                                    buttonPadAnswers.append("8")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 8
+
+                                }
+                                    .buttonPadStyle()
+                                Button("9") {
+                                    buttonPadAnswers.append("9")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 9
+
+                                }
+                                .buttonPadStyle()
+                            }
+                            HStack (alignment: .center, spacing: 20) {
+                             
+                                Button("0") {
+                                    buttonPadAnswers.append("0")
+                                    let answerStr = buttonPadAnswers.joined(separator: "")
+                                    answer = Int(answerStr) ?? 0
+
+                                    print(answer)
+                                }
+                                .buttonPadStyle()
+                                Button("Enter") {
+                                    checkAnswer()
+                                    buttonPadAnswers = [""]
+                                }
+                                .buttonPadStyle()
+                               
+                            }
+                        
                     }
-                    .alert(isPresented: $showingScore) {
-                        Alert(
-                            title: Text(answer == answers[questionNumber] ? "Correct" : "Wrong"),
-                            message: Text("You have \(score) points in total"),
-                            dismissButton: .default(Text("Continue")) {
-                                nextQuestion()
-                            })
-                    }
-                    
-                    Spacer()
-                    
-                    
-                    VStack (spacing: 20) {
-                        HStack (spacing: 20) {
-                            Button("1") {
-                                
-                                buttonPadAnswers.append("1")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 1
-
-                            }
-                                .buttonPadStyle()
-                            Button("2") {
-                                
-                                buttonPadAnswers.append("2")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 2
-                            }
-                                .buttonPadStyle()
-                            Button("3") {
-                                buttonPadAnswers.append("3")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 3
-                            }
-                                .buttonPadStyle()
-                        }
-                        HStack (spacing: 20 ){
-                            Button("4") {
-                                buttonPadAnswers.append("4")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 4
-
-                            }
-                                .buttonPadStyle()
-                            Button("5") {
-                                buttonPadAnswers.append("5")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 5
-
-                            }
-                                .buttonPadStyle()
-                            Button("6") {
-                                buttonPadAnswers.append("6")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 6
-
-                            }
-                                .buttonPadStyle()
-                        }
-                        HStack (spacing: 20) {
-                            Button("7") {
-                                buttonPadAnswers.append("7")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 7
-
-                            }
-                                .buttonPadStyle()
-                            Button("8") {
-                                buttonPadAnswers.append("8")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 8
-
-                            }
-                                .buttonPadStyle()
-                            Button("9") {
-                                buttonPadAnswers.append("9")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 9
-
-                            }
-                            .buttonPadStyle()
-                        }
-                        HStack (alignment: .center, spacing: 20) {
-                         
-                            Button("0") {
-                                buttonPadAnswers.append("0")
-                                let answerStr = buttonPadAnswers.joined(separator: "")
-                                answer = Int(answerStr) ?? 0
-
-                                print(answer)
-                            }
-                            .buttonPadStyle()
-                            Button("Enter") {
-                                checkAnswer()
-                                buttonPadAnswers = [""]
-                            }
-                            .buttonPadStyle()
-                           
-                        }
+                  
                     }
 
                     
@@ -253,8 +276,19 @@ struct ContentView: View {
     }
     
     func startNewGame() {
+        gameStarted = true
         score = 0
         generateQuestions(for: chosenTimesTable, with: totalQuestions)
+//        var qs = Questions()
+//        qs.generateQuestions()
+//        print(qs.questions)
+//        print(qs.answers)
+//        questions = qs.questions
+//        answers = qs.answers
+        
+        
+        
+        
         questionNumber = Int.random(in: 0..<totalQuestions)
     }
 }
@@ -264,3 +298,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
